d0febbe282a1ccca1be3b37c5414ec3b8dcca13e
X-Anas-X <anas.zain1996@gmail.com>
2020-05-11 23:13:09 +0300

    done'

--- code-challenges/includes/.includes(), .every(), .charAt().js
+++ code-challenges/includes/.includes(), .every(), .charAt().js
@@ -2,131 +2,129 @@
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 1
-
-Write a function named firstLetters that takes in an array of strings and returns an array containing only the first letter of each string.
-
+Write a function named firstLetters that takes in an array of strings and
+returns an array containing only the first letter of each string.
 For example, ['this is great :)', 'wow', 'whyyyyyy :(', ':)))))'] returns ['t', 'w', 'w', ':']
 ------------------------------------------------------------------------------------------------ */
 
 const firstLetters = (arr) => {
   // Solution code here...
-  let letters = arr.map( value => {
-    return value.charAt(0);
-  });
-  return letters
+  return arr.map(e => {
+    return e.charAt(0);
+  })
 };
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 2
-
-Write a function named findHappiness that takes in an array of strings and returns an array containing only the strings from the input array that contain ":)".
-
+Write a function named findHappiness that takes in an array of strings and
+returns an array containing only the strings from the input array that contain ":)".
 For example, ['this is great :)', 'wow', 'whyyyyyy :(', ':)))))'] returns ['this is great :)', ':)))))']
 ------------------------------------------------------------------------------------------------ */
 
 const findHappiness = (arr) => {
   // Solution code here...
-  let valArr = [];
-  arr.map(data => {
-    if (data.includes(':)')){
-      valArr.push(data);
-    }
-  });
-  return valArr;
+  return arr.filter(e => {
+    return e.includes(':)')
+  })
 };
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 3
-
-Write a function named standardizePhoneNumbers that takes in an array of phone number strings in (XXX) XXX-XXXX format and returns an array with the phone number strings in XXXXXXXXXX format.
-
+Write a function named standardizePhoneNumbers that takes in an array of phone number
+strings in (XXX) XXX-XXXX format and returns an array with the phone number strings in XXXXXXXXXX format.
 For example, (123) 456-7890 returns 1234567890
 ------------------------------------------------------------------------------------------------ */
 
 const standardizePhoneNumbers = (arr) => {
   // Solution code here...
-  let num = arr.map( data => {
-    data.substring(1, 4) + data.substring(6,9) + data.substring(10, 14)
+  return arr.map(e => {
+    return e.substring(1,4)+e.substring(6,9)+e.substring(10,15);
   })
-  return num;
 };
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 4
-
 Write a function named onlyOddChars that takes in a string and returns only the odd-index characters from that string.
-
 For example, 'abcdefg' returns 'bdf'
 ------------------------------------------------------------------------------------------------ */
 
 const onlyOddChars = (str) => {
   // Solution code here...
-  let oddIdx = [];
-  let letters = str.split('')
-  letters.map( (value, index) => {
-    if (index % 2 !== 0){
-      oddIdx.push(value);
-    }
-  })
-  
-  return oddIdx.join('');
+  let newStr ='';
+  for(let i =1; i< str.length ;i+=2){
+    newStr += str.charAt(i);
+  }
+  return newStr;
 };
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 5
-
-Write a function named allHappy that takes in an array of strings and returns a Boolean indicating whether all those strings contain ":)".
+Write a function named allHappy that takes in an array of strings and returns a Boolean
+indicating whether all those strings contain ":)".
 ------------------------------------------------------------------------------------------------ */
 
 const allHappy = (arr) => {
   // Solution code here...
+  return arr.reduce((acc,e) => {
+    acc = acc & e.includes(':)');
+    return Boolean(acc);
+  },Boolean(1))
 };
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 6
-
-Write a function named findAnything that takes in an array of strings, along with a target string. Return an array containing only those strings from the original array that contain the target string.
+Write a function named findAnything that takes in an array of strings, along with a target string.
+Return an array containing only those strings from the original array that contain the target string.
 ------------------------------------------------------------------------------------------------ */
 
 const findAnything = (arr, target) => {
   // Solution code here...
+  return arr.filter(e => {
+    return e.includes(target);
+  })
 };
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 7
-
-Write a function named findEvery that takes in an array of strings, along with a target string. Return a Boolean based on whether or not every string in the array contains the target string.
+Write a function named findEvery that takes in an array of strings, along with a target string.
+Return a Boolean based on whether or not every string in the array contains the target string.
 ------------------------------------------------------------------------------------------------ */
 
 const findEvery = (arr, target) => {
   // Solution code here...
+  return arr.reduce((acc,e) => {
+    acc= acc & e.includes(target);
+    return Boolean(acc);
+  },Boolean(1));
 };
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 8
-
-We've been testing a new course enrollment system, and we think we have the bugs worked out, but in the meantime, Brook enrolled himself in a bunch of different classes to test if it was working.
-
-Write a function named unenrollBrook that takes in a two-dimensional array, where each array represents one course's roster and is an array of strings of the names of the people in that course.
-
-Return a two-dimensional array with the same roster, but where anyone whose name includes Brook is removed from every course.
-
-For example, [['Brook Testing', 'Actual Person'], ['Human Person', 'Brook again', 'still Brook']] returns [['Actual Person'], ['Human Person']]
+We've been testing a new course enrollment system, and we think we have the bugs worked out, but in the meantime,
+Brook enrolled himself in a bunch of different classes to test if it was working.
+Write a function named unenrollBrook that takes in a two-dimensional array,
+where each array represents one course's roster and is an array of strings of the names of the people in that course.
+Return a two-dimensional array with the same roster,
+but where anyone whose name includes Brook is removed from every course.
+For example, [['Brook Testing', 'Actual Person'], ['Human Person', 'Brook again', 'still Brook']]
+returns [['Actual Person'], ['Human Person']]
 ------------------------------------------------------------------------------------------------ */
 
 const unenrollBrook = (arr) => {
   // Solution code here...
+  return arr.map(e => {
+    return e.filter(val => {
+      return !val.includes('Brook');
+    })
+  })
 };
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 9 - Stretch Goal
-
 Write a function named sortByDay that takes in an array of strings, each of which represents an event's day and time.
-
-Return a two-dimensional array that organizes those strings based on the day on which they occur. For example, all events on Monday are in the first array, all events on Tuesday are in the second array, etc.
-
+Return a two-dimensional array that organizes those strings based on the day on which they occur.
+For example, all events on Monday are in the first array, all events on Tuesday are in the second array, etc.
 If an event takes place on multiple days (i.e. "Dancing on Mondays and Tuesdays"), it should appear in both arrays.
-
 For example, ['Tuesday', 'Monday', 'Wednesday and Thursday', 'Tuesday 2', 'Thursday'] returns
 [
   ['Monday'],
@@ -143,29 +141,38 @@ const daysOfWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Sat
 
 const sortByDay = (arr) => {
   // Solution code here...
+  let newArr = [ [],[],[],[],[],[],[] ];
+
+  daysOfWeek.forEach((element,idx) => {
+    arr.forEach(e => {
+      if(e.includes(element)){
+        newArr[idx].push(e);
+      }
+    });
+  });
+  return newArr;
+
 };
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 10 - Stretch Goal
-
-Write a function named characterByIndex that takes in an array of strings and returns an array containing the first character of the first string, the second character of the second string, etc.
-
+Write a function named characterByIndex that takes in an array of strings and returns an array containing
+the first character of the first string, the second character of the second string, etc.
 For example, ['abcd', 'efgh', 'ijkl', 'mnop'] returns ['a', 'f', 'k', 'p']
 ------------------------------------------------------------------------------------------------ */
 
 const characterByIndex = (arr) => {
   // Solution code here...
+  return arr.map((e,idx) => {
+    return e.charAt(idx);
+  })
 };
 
 /* ------------------------------------------------------------------------------------------------
 TESTS
-
 All the code below will verify that your functions are working to solve the challenges.
-
 DO NOT CHANGE any of the below code.
-
 Run your tests from the console: jest challenges-13.test.js
-
 ------------------------------------------------------------------------------------------------ */
 
 describe('Testing challenge 1', () => {
